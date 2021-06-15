6ac9327f340c9ca59f6dbd1128a0af6e9e7762b1
tinaxdchg8c <chongzhuob079988@yeah.net>
2021-06-16 07:53:45 +0800

    '圆形图片'

---
+++ app/src/main/java/com/tools/bitmap/MainActivityFragment.java
@@ -0,0 +1,58 @@
+package com.tools.bitmap;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.tools.bitmap.pictest.PictestMainActivity;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment implements AdapterView.OnItemClickListener {
+
+    private View view;
+
+    private ListView listView;
+
+    private ArrayAdapter<String> adapter;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_main, container, false);
+        init();
+        return view;
+    }
+
+    private void init() {
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_expandable_list_item_1);
+        adapter.add("拍照和相册选择图片裁剪后更换头像demo");
+        listView = (ListView) view.findViewById(R.id.listView);
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Class<?> cls = null;
+        switch (position) {
+            case 0:
+                cls = PictestMainActivity.class;
+                break;
+        }
+        if (cls != null) {
+            Intent intent = new Intent();
+            intent.setClass(getActivity(), cls);
+            startActivity(intent);
+        }
+    }
+}
