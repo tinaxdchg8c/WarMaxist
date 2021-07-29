26b6ea3256fdb2f4f7648d845161c9cb1463b1e1
tinaxdchg8c <chongzhuob079988@yeah.net>
2021-07-29 08:35:16 +0800

    '0099'

--- ImageAibumTools/src/main/java/com/uwo/tools/aibum/imagescan/ScanMainActivity.java
+++ ImageAibumTools/src/main/java/com/uwo/tools/aibum/imagescan/ScanMainActivity.java
@@ -28,149 +28,131 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * @blog http://blog.csdn.net/xiaanming
- * 
  * @author xiaanming
- * 
- *
+ * @blog http://blog.csdn.net/xiaanming
  */
 public class ScanMainActivity extends Activity {
-	private HashMap<String, List<String>> mGruopMap = new HashMap<String, List<String>>();
-	private List<ImageBean> list = new ArrayList<ImageBean>();
-	private final static int SCAN_OK = 1;
-	private ProgressDialog mProgressDialog;
-	private GroupAdapter adapter;
-	private GridView mGroupGridView;
-	
-	private Handler mHandler = new Handler(){
+    private HashMap<String, List<String>> mGruopMap = new HashMap<String, List<String>>();
+    private List<ImageBean> list = new ArrayList<ImageBean>();
+    private final static int SCAN_OK = 1;
+    private ProgressDialog mProgressDialog;
+    private GroupAdapter adapter;
+    private GridView mGroupGridView;
 
-		@Override
-		public void handleMessage(Message msg) {
-			super.handleMessage(msg);
-			switch (msg.what) {
-			case SCAN_OK:
-				//关闭进度条
-				mProgressDialog.dismiss();
-				
-				adapter = new GroupAdapter(ScanMainActivity.this, list = subGroupOfImage(mGruopMap), mGroupGridView);
-				mGroupGridView.setAdapter(adapter);
-				break;
-			}
-		}
-		
-	};
+    private Handler mHandler = new Handler() {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_scan_main);
-		
-		mGroupGridView = (GridView) findViewById(R.id.main_grid);
-		
-		getImages();
-		
-		mGroupGridView.setOnItemClickListener(new OnItemClickListener() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case SCAN_OK:
+                    //关闭进度条
+                    mProgressDialog.dismiss();
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				List<String> childList = mGruopMap.get(list.get(position).getFolderName());
-				
-				Intent mIntent = new Intent(ScanMainActivity.this, ShowImageActivity.class);
-				mIntent.putStringArrayListExtra("data", (ArrayList<String>)childList);
-				startActivity(mIntent);
-				
-			}
-		});
-		
-	}
+                    adapter = new GroupAdapter(ScanMainActivity.this, list = subGroupOfImage(mGruopMap), mGroupGridView);
+                    mGroupGridView.setAdapter(adapter);
+                    break;
+            }
+        }
 
+    };
 
-	/**
-	 * 利用ContentProvider扫描手机中的图片，此方法在运行在子线程中
-	 */
-	private void getImages() {
-		if(!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
-			Toast.makeText(this, "暂无外部存储", Toast.LENGTH_SHORT).show();
-			return;
-		}
-		
-		//显示进度条
-		mProgressDialog = ProgressDialog.show(this, null, "正在加载...");
-		
-		new Thread(new Runnable() {
-			
-			@Override
-			public void run() {
-				Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-				ContentResolver mContentResolver = ScanMainActivity.this.getContentResolver();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan_main);
 
-				//只查询jpeg和png的图片
-				Cursor mCursor = mContentResolver.query(mImageUri, null,
-						MediaStore.Images.Media.MIME_TYPE + "=? or "
-								+ MediaStore.Images.Media.MIME_TYPE + "=?",
-						new String[] { "image/jpeg", "image/png" }, MediaStore.Images.Media.DATE_MODIFIED);
-				
-				while (mCursor.moveToNext()) {
-					//获取图片的路径
-					String path = mCursor.getString(mCursor
-							.getColumnIndex(MediaStore.Images.Media.DATA));
-					
-					//获取该图片的父路径名
-					String parentName = new File(path).getParentFile().getName();
+        mGroupGridView = (GridView) findViewById(R.id.main_grid);
 
-					
-					//根据父路径名将图片放入到mGruopMap中
-					if (!mGruopMap.containsKey(parentName)) {
-						List<String> chileList = new ArrayList<String>();
-						chileList.add(path);
-						mGruopMap.put(parentName, chileList);
-					} else {
-						mGruopMap.get(parentName).add(path);
-					}
-				}
-				
-				mCursor.close();
-				
-				//通知Handler扫描图片完成
-				mHandler.sendEmptyMessage(SCAN_OK);
-				
-			}
-		}).start();
-		
-	}
-	
-	
-	/**
-	 * 组装分组界面GridView的数据源，因为我们扫描手机的时候将图片信息放在HashMap中
-	 * 所以需要遍历HashMap将数据组装成List
-	 * 
-	 * @param mGruopMap
-	 * @return
-	 */
-	private List<ImageBean> subGroupOfImage(HashMap<String, List<String>> mGruopMap){
-		if(mGruopMap.size() == 0){
-			return null;
-		}
-		List<ImageBean> list = new ArrayList<ImageBean>();
-		
-		Iterator<Map.Entry<String, List<String>>> it = mGruopMap.entrySet().iterator();
-		while (it.hasNext()) {
-			Map.Entry<String, List<String>> entry = it.next();
-			ImageBean mImageBean = new ImageBean();
-			String key = entry.getKey();
-			List<String> value = entry.getValue();
-			
-			mImageBean.setFolderName(key);
-			mImageBean.setImageCounts(value.size());
-			mImageBean.setTopImagePath(value.get(0));//获取该组的第一张图片
-			
-			list.add(mImageBean);
-		}
-		
-		return list;
-		
-	}
+        getImages();
+
+        mGroupGridView.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                List<String> childList = mGruopMap.get(list.get(position).getFolderName());
+
+                Intent mIntent = new Intent(ScanMainActivity.this, ShowImageActivity.class);
+                mIntent.putStringArrayListExtra("data", (ArrayList<String>) childList);
+                startActivity(mIntent);
+            }
+        });
+    }
+
+    /**
+     * 利用ContentProvider扫描手机中的图片，此方法在运行在子线程中
+     */
+    private void getImages() {
+        if (!Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
+            Toast.makeText(this, "暂无外部存储", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //显示进度条
+        mProgressDialog = ProgressDialog.show(this, null, "正在加载...");
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                ContentResolver mContentResolver = ScanMainActivity.this.getContentResolver();
+
+                //只查询jpeg和png的图片
+                Cursor mCursor = mContentResolver.query(mImageUri, null, MediaStore.Images.Media.MIME_TYPE + "=? or " + MediaStore.Images.Media.MIME_TYPE + "=?", new String[]{"image/jpeg", "image/png"}, MediaStore.Images.Media.DATE_MODIFIED);
+
+                while (mCursor.moveToNext()) {
+                    //获取图片的路径
+                    String path = mCursor.getString(mCursor.getColumnIndex(MediaStore.Images.Media.DATA));
+
+                    //获取该图片的父路径名
+                    String parentName = new File(path).getParentFile().getName();
+
+                    //根据父路径名将图片放入到mGruopMap中
+                    if (!mGruopMap.containsKey(parentName)) {
+                        List<String> chileList = new ArrayList<String>();
+                        chileList.add(path);
+                        mGruopMap.put(parentName, chileList);
+                    } else {
+                        mGruopMap.get(parentName).add(path);
+                    }
+                }
+                mCursor.close();
+
+                //通知Handler扫描图片完成
+                mHandler.sendEmptyMessage(SCAN_OK);
+            }
+        }).start();
+    }
 
 
+    /**
+     * 组装分组界面GridView的数据源，因为我们扫描手机的时候将图片信息放在HashMap中
+     * 所以需要遍历HashMap将数据组装成List
+     *
+     * @param mGruopMap
+     * @return
+     */
+    private List<ImageBean> subGroupOfImage(HashMap<String, List<String>> mGruopMap) {
+        if (mGruopMap.size() == 0) {
+            return null;
+        }
+        List<ImageBean> list = new ArrayList<ImageBean>();
+
+        Iterator<Map.Entry<String, List<String>>> it = mGruopMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<String, List<String>> entry = it.next();
+            ImageBean mImageBean = new ImageBean();
+            String key = entry.getKey();
+            List<String> value = entry.getValue();
+
+            mImageBean.setFolderName(key);
+            mImageBean.setImageCounts(value.size());
+            mImageBean.setTopImagePath(value.get(0));//获取该组的第一张图片
+
+            list.add(mImageBean);
+        }
+        return list;
+    }
 }
--- app/src/main/java/com/tools/bitmap/getphoto/GetPhotoMainActivity.java
+++ app/src/main/java/com/tools/bitmap/getphoto/GetPhotoMainActivity.java
@@ -4,95 +4,117 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 import com.tools.bitmap.R;
 
+import java.io.File;
+
 public class GetPhotoMainActivity extends Activity {
 
-	private ImageView iv;
+    private ImageView iv;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_getphoto_main);
-		
-		iv = (ImageView) findViewById(R.id.iv);
-		
-		findViewById(R.id.btn).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				showImagePickDialog();
-			}
-		});
-	}
-	
-	public void showImagePickDialog() {
-		String title = "获取图片方式";
-		String[] choices = new String[]{"拍照", "从手机中选择"};
-		
-		new AlertDialog.Builder(this)
-			.setTitle(title)
-			.setItems(choices, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int which) {
-					dialog.dismiss();
-					switch (which) {
-					case 0:
-						ImageUtils.openCameraImage(GetPhotoMainActivity.this);
-						break;
-					case 1:
-						ImageUtils.openLocalImage(GetPhotoMainActivity.this);
-						break;
-					}
-				}
-			})
-			.setNegativeButton("返回", null)
-			.show();
-	}
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (resultCode == RESULT_CANCELED) {
-			return;
-		}
-		
-		switch (requestCode) {
-		// 拍照获取图片
-		case ImageUtils.GET_IMAGE_BY_CAMERA:
-			// uri传入与否影响图片获取方式,以下二选一
-			// 方式一,自定义Uri(ImageUtils.imageUriFromCamera),用于保存拍照后图片地址
-			if(ImageUtils.imageUriFromCamera != null) {
-				// 可以直接显示图片,或者进行其他处理(如压缩或裁剪等)
-//				iv.setImageURI(ImageUtils.imageUriFromCamera);
-				// 对图片进行裁剪
-				ImageUtils.cropImage(this, ImageUtils.imageUriFromCamera);
-				break;
-			}
-			
-			break;
-		// 手机相册获取图片
-		case ImageUtils.GET_IMAGE_FROM_PHONE:
-			if(data != null && data.getData() != null) {
-				// 可以直接显示图片,或者进行其他处理(如压缩或裁剪等)
-				// iv.setImageURI(data.getData());
-				
-				// 对图片进行裁剪
-				ImageUtils.cropImage(this, data.getData());
-			}
-			break;
-		// 裁剪图片后结果
-		case ImageUtils.CROP_IMAGE:
-			if(ImageUtils.cropImageUri != null) {
-				// 可以直接显示图片,或者进行其他处理(如压缩等)
-				iv.setImageURI(ImageUtils.cropImageUri);
-			}
-			break;
-		default:
-			break;
-		}
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_getphoto_main);
+
+        iv = (ImageView) findViewById(R.id.iv);
+
+        findViewById(R.id.btn).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showImagePickDialog();
+            }
+        });
+    }
+
+    public void showImagePickDialog() {
+        String title = "获取图片方式";
+        String[] choices = new String[]{"拍照", "从手机中选择"};
+
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setItems(choices, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        switch (which) {
+                            case 0:
+                                ImageBitmapUtils.openCameraImage(GetPhotoMainActivity.this);
+                                break;
+                            case 1:
+                                ImageBitmapUtils.openLocalImage(GetPhotoMainActivity.this);
+                                break;
+                        }
+                    }
+                })
+                .setNegativeButton("返回", null)
+                .show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        switch (requestCode) {
+            // 拍照获取图片
+            case ImageBitmapUtils.GET_IMAGE_BY_CAMERA:
+                // uri传入与否影响图片获取方式,以下二选一
+                // 方式一,自定义Uri(ImageBitmapUtils.imageUriFromCamera),用于保存拍照后图片地址
+                if (ImageBitmapUtils.imageUriFromCamera != null) {
+
+                    // 可以直接显示图片,或者进行其他处理(如压缩或裁剪等)
+                    iv.setImageURI(ImageBitmapUtils.imageUriFromCamera);
+                    // 对图片进行裁剪
+				ImageBitmapUtils.cropImage(this, ImageBitmapUtils.imageUriFromCamera);
+
+
+//				Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+//				ImageView imageView = (ImageView) findViewById(R.id.image);
+//				imageView.setImageBitmap(bitmap);
+                    break;
+                }
+                break;
+            // 手机相册获取图片
+            case ImageBitmapUtils.GET_IMAGE_FROM_PHONE:
+                if (data != null && data.getData() != null) {
+                    // 可以直接显示图片,或者进行其他处理(如压缩或裁剪等)
+                    // iv.setImageURI(data.getData());
+
+                    // 对图片进行裁剪
+                    ImageBitmapUtils.cropImage(this, data.getData());
+                }
+                break;
+            // 裁剪图片后结果
+            case ImageBitmapUtils.CROP_IMAGE:
+                if (ImageBitmapUtils.cropImageUri != null) {
+                    // 可以直接显示图片,或者进行其他处理(如压缩等)
+                    iv.setImageURI(ImageBitmapUtils.cropImageUri);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void uriToFilepath(Uri uri){
+        String[] proj = {MediaStore.Images.Media.DATA};
+        Cursor cursor = managedQuery(uri, proj, null, null, null);
+        int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String img_path = cursor.getString(index);
+        File file = new File(img_path);
+        Log.e("file", file.isFile() + file.getPath());
+    }
 }
